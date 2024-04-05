5edb9f024c46caccf11876ea4918a772fdb49bcb
Viktor Mitts <vikezor@disroot.org>
2024-04-05 21:58:36 +0300

    sort out the input a bit, but we should probably use the vector thing

--- Teekkari.gd
+++ Teekkari.gd
@@ -8,11 +8,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	var target_velocity = 0
 	if Input.is_action_pressed("MoveLeft"):
-		$Pelvis.motor_target_velocity = 200
+		target_velocity += 200
+	if Input.is_action_pressed("MoveRight"):
+		target_velocity += -200
+	if target_velocity != 0:
 		$Pelvis.motor_enabled = true
-	elif Input.is_action_pressed("MoveRight"):
-		$Pelvis.motor_target_velocity = -200
-		$Pelvis.motor_enabled = true
+		$Pelvis.motor_target_velocity = target_velocity
 	else:
 		$Pelvis.motor_enabled = false
+	
