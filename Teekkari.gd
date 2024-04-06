d66432281fad03c58c1555e41d8501d6ed410299
Viktor Mitts <vikezor@disroot.org>
2024-04-07 01:52:32 +0300

    oh of course. it's better now but still a bit off somehow

--- Teekkari.gd
+++ Teekkari.gd
@@ -2,15 +2,15 @@ extends RigidBody2D
 
 
 @onready var mouse_pos: Vector2 = get_viewport().get_mouse_position()
-@onready var shoulder_pos: Vector2 = to_viewport_coordinates($Arm/CollisionShape2D/Shoulder.position)
-@onready var hand_pos: Vector2 = to_viewport_coordinates($Arm/CollisionShape2D/Marker2D.position)
+@onready var shoulder_pos: Vector2 = get_viewport_coordinates($Arm/CollisionShape2D/Shoulder)
+@onready var hand_pos: Vector2 = get_viewport_coordinates($Arm/CollisionShape2D/Marker2D)
 @onready var mouse_angle = shoulder_pos.angle_to_point(mouse_pos)
 @onready var arm_angle = shoulder_pos.angle_to_point(hand_pos)
 @onready var diff = mouse_angle - arm_angle
 
 
-func to_viewport_coordinates(vec: Vector2):
-	return get_viewport().get_screen_transform() * get_global_transform_with_canvas() * vec
+func get_viewport_coordinates(node: Node2D):
+	return node.get_viewport().get_screen_transform() * node.get_global_transform_with_canvas() * node.position
 
 
 # Called when the node enters the scene tree for the first time.
@@ -20,8 +20,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	shoulder_pos = to_viewport_coordinates($Arm/CollisionShape2D/Shoulder.position)
-	hand_pos = to_viewport_coordinates($Arm/CollisionShape2D/Marker2D.position)
+	shoulder_pos = get_viewport_coordinates($Arm/CollisionShape2D/Shoulder)
+	hand_pos = get_viewport_coordinates($Arm/CollisionShape2D/Marker2D)
 	var target_velocity = 0
 	if Input.is_action_pressed("MoveLeft"):
 		target_velocity += 200
