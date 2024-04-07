e4a325216a72cb36aa473753274b688943928f7a
Viktor Mitts <vikezor@disroot.org>
2024-04-07 13:55:27 +0300

    i think we can consider this done now

--- Teekkari.gd
+++ Teekkari.gd
@@ -2,8 +2,8 @@ extends RigidBody2D
 
 
 @onready var mouse_pos: Vector2 = get_viewport().get_mouse_position()
-@onready var shoulder_pos: Vector2 = get_viewport_coordinates($Arm/CollisionShape2D/Shoulder)
-@onready var hand_pos: Vector2 = get_viewport_coordinates($Arm/CollisionShape2D/Marker2D)
+@onready var shoulder_pos: Vector2 = get_viewport_coordinates($Arm/Shoulder)
+@onready var hand_pos: Vector2 = get_viewport_coordinates($Arm/Marker2D)
 @onready var mouse_angle = shoulder_pos.angle_to_point(mouse_pos)
 @onready var arm_angle = shoulder_pos.angle_to_point(hand_pos)
 @onready var diff = mouse_angle - arm_angle
@@ -20,8 +20,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	shoulder_pos = get_viewport_coordinates($Arm/CollisionShape2D/Shoulder)
-	hand_pos = get_viewport_coordinates($Arm/CollisionShape2D/Marker2D)
+	shoulder_pos = get_viewport_coordinates($Arm/Shoulder)
+	hand_pos = get_viewport_coordinates($Arm/Marker2D)
 	var target_velocity = 0
 	if Input.is_action_pressed("MoveLeft"):
 		target_velocity += 200
@@ -38,7 +38,7 @@ func _process(_delta):
 	diff = mouse_angle - arm_angle
 	if abs(diff) >= PI:
 		diff += (2 * PI * -sign(diff))
-	$Arm/CollisionShape2D/Shoulder.motor_target_velocity = 360 / PI * diff
+	$Arm/Shoulder.motor_target_velocity = 360 / PI * diff
 
 
 func _input(event):
