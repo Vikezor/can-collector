08594c7efea5e41e7a0afafcaeb1764409406d78
Viktor Mitts <vikezor@disroot.org>
2024-04-07 18:10:23 +0300

    wip can-on-ground checking

--- can/can.gd
+++ can/can.gd
@@ -2,5 +2,5 @@ extends Area2D
 
 
 func _on_area_entered(area):
-	if area is Bag:
+	if area is Bag and not get_parent().get_colliding_bodies().any(func(node): return node.name == "Ground"):
 		(area as Bag).can_collected.emit(get_parent())
